6070c16ad33514b6484d3ee1492d983015a790b0
Anderswith <anderswith@hotmail.com>
2025-03-14 14:24:58 +0100

    .

--- CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
+++ CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36bdd4e227e559d2276a0d6d700a20ed193d39c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98dffd339286a840e90c2918292d9d15a7cc95b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DevOpsCalculator/Program.cs
+++ DevOpsCalculator/Program.cs
@@ -12,7 +12,8 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
+        
+        builder.WebHost.UseUrls("http://0.0.0.0:5062");
         builder.Services.AddControllers();
         builder.Services.AddAuthorization();
         builder.Services.AddEndpointsApiExplorer();
