3f3c40747b4f66bba6b932b4b2e6fcf38a2845b3
Anderswith <anderswith@hotmail.com>
2025-03-14 02:05:28 +0100

    Update Calculation.cs

--- DevOpsCalculator/BE/Calculation.cs
+++ DevOpsCalculator/BE/Calculation.cs
@@ -1,19 +1,22 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace DevOpsCalculator.BE;
 
 public class Calculation
 {
-    [Key]
-    public Guid CalculationId { get; set; }
+    [Key] public Guid CalculationId { get; set; }
     public String CalcString { get; set; }
+
+    [JsonRequired]
+    [Required(ErrorMessage = "A is required")]
     public int? A { get; set; }
+    
+    [JsonRequired]
+    [Required(ErrorMessage = "B is required")]
     public int? B { get; set; }
+
     public int Result { get; set; }
-    
+
     public string? Operation { get; }
-    
-    
-    
-    
 }
