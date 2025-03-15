6fd1dd6824d1958b79fc118c02dc2e5e1ea5a90b
Malene1603 <143804625+Malene1603@users.noreply.github.com>
2025-03-15 22:06:30 +0100

    changed path

--- CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
+++ CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ef08f10ae095bf1d9b5348f5e8221ec7d38c553")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ce6ebe8020f60ffab310c9b2d3a84c61027ccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
