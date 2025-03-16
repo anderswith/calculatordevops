4d054dcb024d1932acb0af7d9fa47acc0ed7f1a4
Malene1603 <143804625+Malene1603@users.noreply.github.com>
2025-03-16 14:06:08 +0100

    n

--- CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
+++ CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21c4d0a9f59a12bc78c6e0b93decc3c3469c814")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9734d1a41fefbcb0c90f36574ac7ec92e6968a3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
