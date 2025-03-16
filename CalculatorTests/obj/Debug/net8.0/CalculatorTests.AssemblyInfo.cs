2fa53cdf1815da99b8d3d8152d8130e40d345cbe
Malene1603 <143804625+Malene1603@users.noreply.github.com>
2025-03-16 14:16:58 +0100

    change

--- CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
+++ CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d054dcb024d1932acb0af7d9fa47acc0ed7f1a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29ea9c491bc8d6ffdde9eb070f2d8818f0d7d83e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
