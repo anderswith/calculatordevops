965df12747254db9db895714f09ded1c90d27915
Malene1603 <143804625+Malene1603@users.noreply.github.com>
2025-03-16 15:02:44 +0100

    changed csproj

--- CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
+++ CalculatorTests/obj/Debug/net8.0/CalculatorTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f6131a65d7b9950710757c4664a29f2be690ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecacff83786f6c4c03624243467be25159fa8741")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
